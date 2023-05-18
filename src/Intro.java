0c60d1403acf85bb9d4f6ebf9244ef8d1d08dbdd
himalayashinde@gmail.com <himalayashinde@gmail.com>
2023-05-18 15:14:02 +0530

    Selenium Browser init

---
+++ src/Intro.java
@@ -0,0 +1,16 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Intro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("web.chrome.driver", System.getProperty("user.dir") + "\\src\\resources\\chromedriver.exe");
+
+		WebDriver driver = new ChromeDriver();
+
+		driver.get("https://www.google.co.in");
+
+	}
+
+}
