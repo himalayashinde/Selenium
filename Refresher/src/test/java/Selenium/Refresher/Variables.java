09dee5b2196d9ec41c6f15e68c59a012871fe03f
himalayashinde@gmail.com <himalayashinde@gmail.com>
2024-04-25 21:56:03 +0530

    Update variables

--- Refresher/src/test/java/Selenium/Refresher/Variables.java
+++ Refresher/src/test/java/Selenium/Refresher/Variables.java
@@ -14,10 +14,55 @@ public class Variables {
 		
 		int c= 300; //instance variable
 		Variables v= new Variables();
+		/*###################################################################*/
 		
 		System.out.println("Static Variable a value: "+ a);
 		v.method();
 		System.out.println("Instance Variable c value: "+c);
+
+		/*###################################################################*/	
+		
+		//Add two numbers
+		
+		int x=10, y=20, z=x+y;
+		System.out.println("Sum :"+ z);
+		
+		/*###################################################################*/
+		
+		//Widening
+		
+		float f =x;
+		System.out.println("Widening: "+ f);
+		
+		/*###################################################################*/
+		
+		//Narrowing or typecasting
+		float fl= 10.5f;
+		int d= (int)fl;
+		
+		System.out.println("Float: "+fl);
+		System.out.println("Typecasted Int : "+d);
+		
+		/*###################################################################*/
+		
+		//Overflow
+		int e=130;
+		byte g= (byte)e;
+		System.out.println("Int: "+ e+ " OverFlow :"+ g);
+		
+		/*###################################################################*/
+		
+		//Adding Lower Types
+		
+		byte ab=20, bc= 20;
+		byte cd= (byte) (ab+bc);
+		System.out.println("Adding lower types : "+ cd);
+		
+		/*###################################################################*/
+		
+		
+		
+		
 	}
 
 }
